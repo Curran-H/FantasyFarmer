2cdf0bb11954a364626648417944995a64543ece
trevorlecroy <trevorlecroy@hotmail.com>
2023-01-28 04:02:52 -0500

    Carried away with player controls and building system. Added a basic but extendable state machine for the player and the ability to place buildings of varying grid widths/heights.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public enum PlayerState { IDLE, PLACING, PLACE }
+    public PlayerState m_playerState;
+
     [SerializeField] private float m_movementSpeed = 5f;
     private Vector3 m_input;
 
+    [SerializeField] private Building m_selectedBuilding;
+    private Vector3 m_mousePosition;
+
+
     private void Update() {
+        m_mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        m_mousePosition = new Vector3(m_mousePosition.x, m_mousePosition.y, 0);
+
         CheckInput();
+        UpdateState();
 
         transform.position += m_input.normalized * m_movementSpeed * Time.deltaTime;
+    }
+
+    private void UpdateState() {
+        switch(m_playerState) {
+            case PlayerState.PLACING:
+                MapManager.Instance.Placing(m_mousePosition, m_selectedBuilding);
+                break;
+        }
+    }
+
+    private void ChangeState(PlayerState newState) {
+        m_playerState = newState;
+        switch(m_playerState) {
+            case PlayerState.IDLE:
+                MapManager.Instance.CancelPlacing();
+                break;
+            case PlayerState.PLACING:
+                MapManager.Instance.StartPlacing(m_mousePosition, m_selectedBuilding);
+                break;
+            case PlayerState.PLACE:
+                if(MapManager.Instance.IsValidPlacement(m_mousePosition, m_selectedBuilding)) {
+                    MapManager.Instance.EndPlacing(m_mousePosition, m_selectedBuilding);
+                    ChangeState(PlayerState.IDLE);
+                } else {
+                    ChangeState(PlayerState.PLACING);
+                }
+                break;
+        }
     }
 
     private void CheckInput() {
@@ -18,5 +57,19 @@ public class PlayerController : MonoBehaviour {
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
         );
+
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            if(m_playerState == PlayerState.IDLE) {
+                ChangeState(PlayerState.PLACING);
+            } else {
+                ChangeState(PlayerState.IDLE);
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0)) {
+            if(m_playerState == PlayerState.PLACING) {
+                ChangeState(PlayerState.PLACE);
+            }
+        }
     }
 }
