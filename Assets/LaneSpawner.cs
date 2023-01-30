fd9656ea26d80066b88e6f51f0de7695303dd233
Okavin <37856827+Shounsandhu@users.noreply.github.com>
2023-01-30 14:28:37 +0200

    added laneSpawner class

---
+++ Assets/LaneSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Enemy EnemyToSpawn;
+    ///<summary> top lane is 0</summary>
+    [SerializeField] private int laneIndex;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
