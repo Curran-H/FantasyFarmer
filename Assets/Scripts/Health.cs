a096a52e7dceebca23e25b4973da5b33a5c57f81
trevorlecroy <trevorlecroy@hotmail.com>
2023-01-30 07:01:31 -0500

    Added basis for Turret/Enemy heatlh system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour {
+    public int MaxHealth { get; protected set; }
+    public int CurrentHealth { get; protected set; }
+    public UnityEvent OnDeathcallback;
+
+    public void Initialize(int maxHealth) {
+        MaxHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount) {
+        CurrentHealth -= amount;
+        if(CurrentHealth <= 0) {
+            OnDeathcallback.Invoke();
+        }
+    }
+}
