74c0a216cd31a0b1b757908a73f97b1eaec2cc8f
Okavin <37856827+Shounsandhu@users.noreply.github.com>
2023-01-31 21:32:50 +0200

    test

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,29 +4,75 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour {
 
+    public int m_waveNumber;
+
     public static WaveManager Instance;
-    public List<GameObject> m_enemy=new List<GameObject>();//fill the list with different enemies and use their index to set what lanes spawn what specific enemy
-    //private List<LaneSpawner> m_laneSpawners= new List<LaneSpawner>();
+    /// <summary>stores indexes of which lanes are to be active</summary>
+    [SerializeField] private List<int> m_activeLaneIndexes = new List<int>();
+
+    ///<summary>how many lanes can spawn enemies at once</summary>
+    [SerializeField] private int m_maxPossibleActiveLanes;
+
+    ///<summary>minumum time to wait between enemy spawns in a lane</summary>
+    [SerializeField] private int m_minSpawnInterval;
+
+    ///<summary>maximum time to wait between enemy spawns in a lane</summary>
+    [SerializeField] private int m_maxSpawnInterval;
+
+    ///<summary>stores integer that states how long to wait before changing which lanes are active</summary>
+    [SerializeField] private int laneSwapInterval;
+
+    private float m_elapsedTime;
+
+    public List<GameObject> m_enemies=new List<GameObject>();//fill the list with different enemies and use their index to set what lanes spawn what specific enemy
+    
     [SerializeField]private LaneSpawner[] m_laneSpawners;
     private void Awake() {
         if(Instance == null) 
             Instance = this;
+        
     }
     private void Start()
     {
+        m_waveNumber = 0;
+        m_elapsedTime = 0;
         m_laneSpawners = new LaneSpawner[transform.childCount];//the lanes are children of wavemanager so create an array that can fit all of them
         InitialiseLaneSpawners();
     }
     private void Update() {
-        m_laneSpawners[0].SetActive(true);
-        m_laneSpawners[0].SetEnemyToSpawn(m_enemy[0]);
+        m_elapsedTime += Time.deltaTime;
+        if (m_elapsedTime > laneSwapInterval)
+        {
+
+        }
         
     }
 
-    public void SpawnWave() {
+    //filling array with laneSpawners
+
+    void SelectRandomActiveLanes()
+    {
+        
+        for(int i = 0; i < m_maxPossibleActiveLanes; i++)
+        {
+            m_activeLaneIndexes.Add((int)Random.Range(0, m_laneSpawners.Length));
+            
+        }
     }
 
-    //filling array with laneSpawners
+    void ActivateLanes()
+    {
+        //deactivate all lanes first
+        foreach(LaneSpawner ls in m_laneSpawners)
+        {
+            ls.SetActive(false);
+        }
+        //now activate the new active lanes
+        for(int i = 0; i < m_activeLaneIndexes.Count; i++)
+        {
+            m_laneSpawners[m_activeLaneIndexes[i]].SetActive(true);
+        }
+    }
     void InitialiseLaneSpawners()
     {
 
