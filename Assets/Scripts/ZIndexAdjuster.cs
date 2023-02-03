37cef79055757c3c098d7baf7e0af04b67dc2383
trevorlecroy <trevorlecroy@hotmail.com>
2023-02-02 23:27:34 -0500

    Added ZIndexAdjuster

---
+++ Assets/Scripts/ZIndexAdjuster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZIndexAdjuster : MonoBehaviour {
+
+    private SpriteRenderer m_spriteRenderer;
+    [SerializeField] private bool m_doUpdate;
+
+    private void Awake() {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        AdjustZIndex();
+    }
+
+    private void Update() {
+        if(m_doUpdate) {
+            AdjustZIndex();
+        }
+    }
+
+    private void AdjustZIndex() {
+        m_spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y);
+    }
+}
