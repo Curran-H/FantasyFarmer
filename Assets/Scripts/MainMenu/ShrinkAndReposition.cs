f496b62e1c4497955b0a1cb1cb0eb6b66192cac2
Curran-H <51970226+Curran-H@users.noreply.github.com>
2023-02-08 16:57:26 -0600

    Squashed commit of the following:
    commit 4dfc64a2b7bdcb7da15e6699c03659cbea347e0f
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:55:28 2023 -0600
    
        Revert "V1.1.1"
    
        This reverts commit f0e3d0871d4d9af32493a64aee8f0fcec7337880.
    
    commit df613392270ed1d68d2852005601592030130f60
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:55:26 2023 -0600
    
        Revert "Fuck you git"
    
        This reverts commit 75fbd1ddeb1b8bf86825f59548889a3fba48e0a6.
    
    commit a523b153394380476beeec0da408efd40d06f96e
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:55:25 2023 -0600
    
        Revert "Update EditorUserSettings.asset"
    
        This reverts commit 1975b7b5959e4646cbbd384ffe1ae3bc8705cdcd.
    
    commit c9f4e3af2a6cc42b429a474561787c8cec8d48c0
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:54:41 2023 -0600
    
        Revert "V1.1"
    
        This reverts commit b1336372a030948ba88c22cf41e710fd33714f78.
    
    commit 1975b7b5959e4646cbbd384ffe1ae3bc8705cdcd
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:44:52 2023 -0600
    
        Update EditorUserSettings.asset
    
    commit 75fbd1ddeb1b8bf86825f59548889a3fba48e0a6
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:41:22 2023 -0600
    
        Fuck you git
    
    commit f0e3d0871d4d9af32493a64aee8f0fcec7337880
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:24:36 2023 -0600
    
        V1.1.1
    
        Please work dear God
    
    commit b1336372a030948ba88c22cf41e710fd33714f78
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:15:37 2023 -0600
    
        V1.1
    
    commit 4908b4c9dfe869f4b0cd04a0ec244f98840a03fd
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Wed Feb 8 16:12:11 2023 -0600
    
        V1 Push
    
        Actually sorted all of the files I added, wowzers
    
        Added title screen, options screen, play game button
    
    commit 0ad75948c8d80acf51cfcb29138dac32f4e3483b
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Tue Feb 7 20:23:15 2023 -0600
    
        Final Effect Script
    
        Checkpoint!
    
    commit 9959112cf94d952bb2bfddb9adaa5681f1acd0d5
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Tue Jan 31 14:22:40 2023 -0600
    
        CHECKPOINT
    
        I'm about to get messy so I'm leaving this as a restore point. Pray for me
    
    commit ad1cee28193f3580a1772f935f93cd19d43286db
    Author: Curran-H <51970226+Curran-H@users.noreply.github.com>
    Date:   Mon Jan 30 17:49:11 2023 -0600
    
        Dissolve shader
    
        Added dissolve shader and title screen emission map

---
+++ Assets/Scripts/MainMenu/ShrinkAndReposition.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkAndReposition : MonoBehaviour
+{
+    public Vector3 startPosition = new Vector3(0, 2, 0);
+    public float startScale = 2;
+
+    public Vector3 targetPosition = new Vector3(0, 1.75f, 0);
+    public float targetScale = 0.75f;
+
+    public float transitionTime = 1.0f;
+    private float timeElapsed = 0.0f;
+
+    public bool shouldShrink = false;
+
+    private void Start()
+    {
+        transform.position = startPosition;
+        transform.localScale = new Vector3(startScale, startScale, startScale);
+    }
+
+    private void Update()
+    {
+        if (shouldShrink)
+        {
+            if (timeElapsed < transitionTime)
+            {
+                timeElapsed += Time.deltaTime;
+
+                float t = timeElapsed / transitionTime;
+                transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+                transform.localScale = Vector3.Lerp(new Vector3(startScale, startScale, startScale), new Vector3(targetScale, targetScale, targetScale), t);
+            }
+            else
+            {
+                shouldShrink = false;
+            }
+        }
+    }
+}
